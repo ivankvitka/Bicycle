e1f62af2a828d32ad6cc0aceb4da6e4b2d8a85ec
ivankvitka <ivan.kvitka@gmail.com>
2018-12-08 10:57:41 +0200

    fix gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,34 +5,34 @@ var plumber = require('gulp-plumber');
 var rename = require('gulp-rename');
 
 gulp.task("sass", function () {
-	gulp
-		.src("app/scss/main.scss")
-		.pipe(plumber())
-		.pipe(sass({outputStyle: "expanded"}))
-		.pipe(rename("main.css"))
-		.pipe(gulp.dest("app/css"))
-		.pipe(browserSync.reload({stream: true}));
+  gulp
+    .src("app/scss/main.scss")
+    .pipe(plumber())
+    .pipe(sass({outputStyle: "expanded"}))
+    .pipe(rename("main.css"))
+    .pipe(gulp.dest("app/css"))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('browser-sync', function () {
-	browserSync({
-		server: {
-			baseDir: 'app'
-		},
-		notify: false
-	});
+  browserSync({
+    server: {
+      baseDir: 'app'
+    },
+    notify: false
+  });
 });
 
 gulp.task('watch', ['browser-sync', 'sass'], function () {
-	gulp.watch('app/scss/**/*.scss', ['sass']);
-	gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload);
 });
 
 gulp.task('build', function () {
-	gulp.src('app/css/*.css')
-		.pipe(gulp.dest('build/css'));
-	gulp.src('app/*.html')
-		.pipe(gulp.dest('build'));
-	gulp.src('app/images/*')
-		.pipe(gulp.dest('build/images'));
+  gulp.src('app/css/*.css')
+    .pipe(gulp.dest('build/css'));
+  gulp.src('app/*.html')
+    .pipe(gulp.dest('build'));
+  gulp.src('app/images/*')
+    .pipe(gulp.dest('build/images'));
 });
